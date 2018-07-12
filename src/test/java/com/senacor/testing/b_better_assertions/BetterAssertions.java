d79864a0b7e56b66af402f34457979231e3fa11b
Michael Omann <michael.omann@gmail.com>
2018-07-12 20:40:57 +0200

    add assertions example

---
+++ src/test/java/com/senacor/testing/b_better_assertions/BetterAssertions.java
@@ -0,0 +1,52 @@
+package com.senacor.testing.b_better_assertions;
+
+import com.senacor.testing.granularity.Email;
+import com.senacor.testing.granularity.Poll;
+import com.senacor.testing.granularity.PollAssert;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+public class BetterAssertions {
+
+
+    @Test
+    public void assertJAssertions() {
+        Poll sut = Poll.newBuilder()
+                .participant("id1", Email.of("1@1.com"))
+                .participant("id2", Email.of("2@2.com"))
+                .build();
+
+        int count = sut.getParticipantCount();
+
+        assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    public void generateAssertionsWithTheMavenPlugin() {
+        Poll sut = Poll.newBuilder()
+                .participant("id1", Email.of("shared-email.com"))
+                .participant("id2", Email.of("shared-email.com"))
+                .build();
+
+        PollAssert.assertThat(sut)
+                .hasOnlyParticipantEmails(Email.of("shared-email.com"));
+    }
+
+    @Test
+    public void hamcrestMatchers() {
+        Poll sut = Poll.newBuilder()
+                .participant("id1", Email.of("1@1.com"))
+                .participant("id2", Email.of("2@2.com"))
+                .build();
+
+        Set<Email> emails = sut.getParticipantEmails();
+
+        Assert.assertThat(emails, containsInAnyOrder(Email.of("1@1.com"), Email.of("2@2.com")));
+    }
+
+}
