1d8baac12ba2001f26d88559d59ed931436e103b
Michael Omann <michael.omann@gmail.com>
2018-07-18 20:40:15 +0200

    add test doubles

---
+++ src/test/java/com/senacor/testing/test_doubles/PollRepositoryStub.java
@@ -0,0 +1,39 @@
+package com.senacor.testing.test_doubles;
+
+import com.senacor.testing.Poll;
+import com.senacor.testing.PollRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PollRepositoryStub extends PollRepository {
+
+    protected final List<Poll> polls = new ArrayList<>();
+
+    public PollRepositoryStub() {
+        super(null);
+    }
+
+    void addPoll(Poll poll) {
+        polls.add(poll);
+    }
+
+    @Override
+    public Optional<Poll> findPoll(String pollId) {
+        return polls.stream().findFirst();
+    }
+
+    @Override
+    public void insert(Poll poll) {
+        addPoll(poll);
+    }
+
+    //UNSUPPORTED OPERATIONS
+
+    @Override
+    public void deleteAll() {
+        throw new UnsupportedOperationException();
+    }
+
+}
